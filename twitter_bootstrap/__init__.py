ad2fe20e5db8c40a3470154152dd8a72857b4c5c
victor <vsobol@maven.co>
2014-10-17 12:03:43 +0000

    Remove obsolete css files and compile bootstrap.css with updated variables

--- twitter_bootstrap/__init__.py
+++ twitter_bootstrap/__init__.py
@@ -1,7 +1,4 @@
 from fanstatic import Library, Resource
 
 library = Library('twitter_bootstrap', 'resources')
-
 twitter_bootstrap = Resource(library, 'bootstrap.css')
-twitter_bootstrap_responsive = Resource(library, 'bootstrap-responsive.css')
-twitter_bootstrap_wide = Resource(library, 'bootstrap-wide.css', depends=[twitter_bootstrap])
