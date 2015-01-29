1a1f24ad309d0a367d4a5d2fe36258d93e75498e
Yuriy Makhnovskyy <ymakhnovskyy@maven.co>
2015-01-29 13:23:59 +0200

    MAVEN-16085 Added bootstrap.css.map file.

--- twitter_bootstrap/__init__.py
+++ twitter_bootstrap/__init__.py
@@ -2,3 +2,4 @@ from fanstatic import Library, Resource
 
 library = Library('twitter_bootstrap', 'resources')
 twitter_bootstrap = Resource(library, 'bootstrap.css')
+twitter_bootstrap_map = Resource(library, 'bootstrap.css.map')
