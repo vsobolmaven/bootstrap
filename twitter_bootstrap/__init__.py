2f9310bcc0eee7447267a15227b7a705afc51a53
Yuriy Makhnovskyy <ymakhnovskyy@maven.co>
2015-01-29 14:01:55 +0200

    Removed map resource.

--- twitter_bootstrap/__init__.py
+++ twitter_bootstrap/__init__.py
@@ -2,4 +2,3 @@ from fanstatic import Library, Resource
 
 library = Library('twitter_bootstrap', 'resources')
 twitter_bootstrap = Resource(library, 'bootstrap.css')
-twitter_bootstrap_map = Resource(library, 'bootstrap.css.map')
